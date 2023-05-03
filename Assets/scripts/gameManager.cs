4c5453465641d20bdf7f4535db018a4a3257fd5d
Shyunju <hyunju9908@naver.com>
2023-05-03 21:26:17 +0900

    card images setting
    image list up & random sprite

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class gameManager : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class gameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i =0; i<16; i++)
+        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();  //리스트 정렬을 랜덤한 순서로 정렬
+
+        for (int i =0; i<16; i++)
         {
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
@@ -19,6 +23,9 @@ public class gameManager : MonoBehaviour
             float x = (i % 4) * 1.4f -2.1f;
             float y = (i / 4) * 1.4f -3f;
             newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
         }
 
         
