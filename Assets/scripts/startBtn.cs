f94b0eeef106a9bcc1044463bd13450d91038db3
Shyunju <hyunju9908@naver.com>
2023-05-07 23:09:22 +0900

    start
    game start button

--- Assets/scripts/startBtn.cs
+++ Assets/scripts/startBtn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class startBtn : MonoBehaviour
 {
@@ -14,5 +15,9 @@ public class startBtn : MonoBehaviour
     void Update()
     {
         
+    }
+    public void gameStart()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
